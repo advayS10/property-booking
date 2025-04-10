148b92eee8e447ce853e497726c9cbffe60b2586
advayS10 <163100260+advayS10@users.noreply.github.com>
2025-04-10 11:20:36 +0530

    remove

--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IAPIResponseModel, IPropertytype, Site } from '../model/master';
-import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
